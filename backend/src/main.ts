161483630121354e212a8f8cb29465397a90dd9d
Mariia <ma.gaiduchenko@gmail.com>
2023-06-22 00:18:07 +0300

    ktm-49: * files to requiring eslint rules

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 
+// eslint-disable-next-line import/extensions
 import { AppModule } from './app.module';
 
+// eslint-disable-next-line function-name/starts-with-verb
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
