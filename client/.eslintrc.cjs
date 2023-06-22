a1e059bf4f92b795413b9295bdb08b5f5e4dec04
Mariia <ma.gaiduchenko@gmail.com>
2023-06-22 13:24:18 +0300

    ktm-49: + cjs to prettier commands

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,35 +1,45 @@
 module.exports = {
   env: { browser: true, es2020: true },
-  settings: {"react": {version: "detect"}},
+  settings: { react: { version: 'detect' } },
   extends: [
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
     'plugin:react/jsx-runtime',
-    "plugin:jsx-a11y/recommended",
-    "../.eslintrc.js"
+    'plugin:jsx-a11y/recommended',
+    '../.eslintrc.js',
   ],
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module', project: './tsconfig.json' },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
   plugins: ['react-refresh'],
   ignorePatterns: ['.eslintrc.cjs'],
   rules: {
     'react-refresh/only-export-components': 'warn',
     'unicorn/filename-case': 'off',
-    "react/self-closing-comp": ["error", {
-      "component": true,
-      "html": true
-    }],
-    "react-hooks/exhaustive-deps": "error",
-    "react/jsx-no-bind": ["error", {
-      "ignoreRefs": true
-    }]
+    'react/self-closing-comp': [
+      'error',
+      {
+        component: true,
+        html: true,
+      },
+    ],
+    'react-hooks/exhaustive-deps': 'error',
+    'react/jsx-no-bind': [
+      'error',
+      {
+        ignoreRefs: true,
+      },
+    ],
   },
   overrides: [
     {
-      files: ["vite-env.d.ts"],
+      files: ['vite-env.d.ts'],
       rules: {
-        'unicorn/prevent-abbreviations': 'off'
-      }
-    }
-  ]
-}
+        'unicorn/prevent-abbreviations': 'off',
+      },
+    },
+  ],
+};
