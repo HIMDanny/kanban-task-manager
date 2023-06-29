485b30900e48c88aee8930861c20046d27f1daf8
HIMDanny <danilbliz0201@gmail.com>
2023-06-29 13:15:26 +0300

    ktm-35: + starter theme provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
 
 import App from './App.tsx';
 import { GlobalStyle } from './assets/styles/global-style.ts';
+import { light } from './assets/styles/themes.ts';
 
 // eslint-disable-next-line @typescript-eslint/non-nullable-type-assertion-style
 ReactDOM.createRoot(document.querySelector('#root') as HTMLElement).render(
   <React.StrictMode>
     <GlobalStyle />
-    <App />
+    <ThemeProvider theme={light}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
