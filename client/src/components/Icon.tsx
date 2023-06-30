3285c02f601d7386dfb5ab75b3675bc1f5661b9f
HIMDanny <danilbliz0201@gmail.com>
2023-06-30 21:18:13 +0300

    ktm-9: * move Icon component

--- client/src/components/Icon.tsx
+++ client/src/components/Icon.tsx
@@ -1,14 +1,14 @@
 import type { IconType } from 'client/src/types/icon';
 
-import { ReactComponent as BoardIcon } from '../../assets/icons/board.svg';
-import { ReactComponent as CancelIcon } from '../../assets/icons/cancel.svg';
-import { ReactComponent as CheckIcon } from '../../assets/icons/check.svg';
-import { ReactComponent as ChevronDownIcon } from '../../assets/icons/chevron-down.svg';
-import { ReactComponent as EyeIcon } from '../../assets/icons/eye.svg';
-import { ReactComponent as EyeSlashIcon } from '../../assets/icons/eye-slash.svg';
-import { ReactComponent as MenuIcon } from '../../assets/icons/menu.svg';
-import { ReactComponent as MoonIcon } from '../../assets/icons/moon.svg';
-import { ReactComponent as SunIcon } from '../../assets/icons/sun.svg';
+import { ReactComponent as BoardIcon } from '../assets/icons/board.svg';
+import { ReactComponent as CancelIcon } from '../assets/icons/cancel.svg';
+import { ReactComponent as CheckIcon } from '../assets/icons/check.svg';
+import { ReactComponent as ChevronDownIcon } from '../assets/icons/chevron-down.svg';
+import { ReactComponent as EyeIcon } from '../assets/icons/eye.svg';
+import { ReactComponent as EyeSlashIcon } from '../assets/icons/eye-slash.svg';
+import { ReactComponent as MenuIcon } from '../assets/icons/menu.svg';
+import { ReactComponent as MoonIcon } from '../assets/icons/moon.svg';
+import { ReactComponent as SunIcon } from '../assets/icons/sun.svg';
 
 interface IconProperties {
   icon: IconType;
