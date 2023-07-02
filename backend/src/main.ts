510f82868dffafb328c54bfad1fdb3f4e43e4b6e
Myroslav Boiko <ma.gaiduchenko@gmail.com>
2023-07-02 14:58:27 +0300

    ktm-56: + logger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import type { NestFastifyApplication } from '@nestjs/platform-fastify';
 import { FastifyAdapter } from '@nestjs/platform-fastify';
+import { Logger } from 'nestjs-pino';
 
 import { AppModule } from './app.module';
 import { setupSwagger } from './libs/packages/swagger/swagger';
@@ -9,8 +10,10 @@ async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
+    { bufferLogs: true },
   );
   setupSwagger(app);
+  app.useLogger(app.get(Logger));
   await app.listen(3000);
 }
 bootstrap();
