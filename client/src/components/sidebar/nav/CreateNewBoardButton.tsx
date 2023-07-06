ed40bb85eafab8e185f8d72480942ad8ba1498a2
HIMDanny <danilbliz0201@gmail.com>
2023-07-06 19:08:05 +0300

    ktm-5: * change element from link to button

--- client/src/components/sidebar/nav/CreateNewBoardButton.tsx
+++ client/src/components/sidebar/nav/CreateNewBoardButton.tsx
@@ -1,31 +1,29 @@
-import { Link } from 'react-router-dom';
 import { styled } from 'styled-components';
 
 import { Icon } from '../../Icon';
 
-// ? can be changed to button instead of link
 const CreateNewBoardButton: React.FC = () => {
   return (
     <ListButtonItem>
-      <Link to="/new">
-        <Icon icon="board" /> <span>+ Create new board</span>
-      </Link>
+      <Icon icon="board" />
+      <span>+ Create new board</span>
     </ListButtonItem>
   );
 };
 export { CreateNewBoardButton };
 
-const ListButtonItem = styled.div`
+const ListButtonItem = styled.button`
   color: ${({ theme }): string => theme.palette.primary.main};
-
-  a {
-    padding: 0.875rem 1.5rem;
-    display: grid;
-    grid-template-columns: 1rem auto;
-    align-items: center;
-    gap: 0.75rem;
-    font-weight: 700;
-  }
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  padding: 0.875rem 1.5rem;
+  display: grid;
+  grid-template-columns: 1rem auto;
+  align-items: center;
+  gap: 0.75rem;
+  font-size: 1rem;
+  font-weight: 700;
 
   &:hover {
     color: ${({ theme }): string => theme.palette.primary.light};
