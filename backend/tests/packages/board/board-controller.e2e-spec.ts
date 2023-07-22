4b0228e78b41f18d4771f69f0a06175b5862d851
Myroslav Boiko <ma.gaiduchenko@gmail.com>
2023-07-22 19:07:48 +0300

    ktm-88: + common constants

--- backend/tests/packages/board/board-controller.e2e-spec.ts
+++ backend/tests/packages/board/board-controller.e2e-spec.ts
@@ -1,4 +1,3 @@
-// import { NotFoundException } from '@nestjs/common/exceptions/not-found.exception';
 // eslint-disable-next-line unicorn/prevent-abbreviations
 import type { NestFastifyApplication } from '@nestjs/platform-fastify';
 import { FastifyAdapter } from '@nestjs/platform-fastify';
@@ -9,6 +8,11 @@ import { BoardModule } from '../../../src/packages/board/board.module';
 describe('BoardController (e2e)', () => {
   let app: NestFastifyApplication;
   let boardId: number;
+
+  const BOARD_ENDPOINT = '/boards';
+  const NON_EXISTENT_BOARD_ID = 999;
+  const testBoard = { name: 'Pohemons' };
+  const updatedBoard = { name: 'Pokemons' };
 
   beforeAll(async () => {
     const moduleReference = await Test.createTestingModule({
@@ -27,65 +31,71 @@ describe('BoardController (e2e)', () => {
     await app.close();
   });
 
-  it('/boards (GET)', async () => {
-    const response = await app.inject({
-      method: 'GET',
-      url: '/boards',
-    });
+  describe(`GET ${BOARD_ENDPOINT}`, () => {
+    it('should return status 200 and boards', async () => {
+      const response = await app.inject({
+        method: 'GET',
+        url: BOARD_ENDPOINT,
+      });
 
-    expect(response.statusCode).toEqual(200);
-    expect(response.payload).toEqual(response.body);
+      expect(response.statusCode).toEqual(200);
+    });
   });
 
-  it('/boards (POST)', async () => {
-    const requestBody = { name: 'Pohemons' };
-    const response = await app.inject({
-      method: 'POST',
-      url: '/boards',
-      payload: requestBody,
-    });
+  describe(`POST ${BOARD_ENDPOINT}`, () => {
+    it('should create a new board', async () => {
+      const response = await app.inject({
+        method: 'POST',
+        url: BOARD_ENDPOINT,
+        payload: testBoard,
+      });
 
-    expect(response.statusCode).toEqual(201);
-    const createdBoard = JSON.parse(response.body);
-    boardId = createdBoard.id;
+      expect(response.statusCode).toEqual(201);
+      const createdBoard = JSON.parse(response.payload);
+      boardId = createdBoard.id;
+    });
   });
 
-  it('/boards/:id (GET)', async () => {
-    const response = await app.inject({
-      method: 'GET',
-      url: `/boards/${boardId}`,
+  describe(`GET ${BOARD_ENDPOINT}/:id`, () => {
+    it('should return status 200 for a valid board', async () => {
+      const response = await app.inject({
+        method: 'GET',
+        url: `${BOARD_ENDPOINT}/${boardId}`,
+      });
+
+      expect(response.statusCode).toEqual(200);
     });
 
-    expect(response.statusCode).toEqual(200);
+    it('should return status 404 for a non-existent board', async () => {
+      const response = await app.inject({
+        method: 'GET',
+        url: `${BOARD_ENDPOINT}/${NON_EXISTENT_BOARD_ID}`,
+      });
+
+      expect(response.statusCode).toEqual(404);
+    });
   });
 
-  it('/boards/:id (GET) - Not found', async () => {
-    const nonExistentId = 999;
-    const response = await app.inject({
-      method: 'GET',
-      url: `/boards/${nonExistentId}`,
-    });
+  describe(`PUT ${BOARD_ENDPOINT}/:id`, () => {
+    it('should update the board', async () => {
+      const response = await app.inject({
+        method: 'PUT',
+        url: `${BOARD_ENDPOINT}/${boardId}`,
+        payload: updatedBoard,
+      });
 
-    expect(response.statusCode).toEqual(404);
+      expect(response.statusCode).toEqual(200);
+    });
   });
 
-  it('/boards/:id (PUT)', async () => {
-    const requestBody = { name: 'Pokemons' };
-    const response = await app.inject({
-      method: 'PUT',
-      url: `/boards/${boardId}`,
-      payload: requestBody,
+  describe(`DELETE ${BOARD_ENDPOINT}/:id`, () => {
+    it('should delete the board', async () => {
+      const response = await app.inject({
+        method: 'DELETE',
+        url: `${BOARD_ENDPOINT}/${boardId}`,
+      });
+
+      expect(response.statusCode).toEqual(200);
     });
-
-    expect(response.statusCode).toEqual(200);
-  });
-
-  it('/boards/:id (DELETE)', async () => {
-    const response = await app.inject({
-      method: 'DELETE',
-      url: `/boards/${boardId}`,
-    });
-
-    expect(response.statusCode).toEqual(200);
   });
 });
